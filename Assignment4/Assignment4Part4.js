4e96ae6cde7b1285dc6f15aab2a3177955e66250
azamzubair <zubair.azam@dcmail.ca>
2023-11-21 23:52:44 -0500

    raw text
    this commit includes the raw text files for part 4 of this assignment

--- Assignment4/Assignment4Part4.js
+++ Assignment4/Assignment4Part4.js
@@ -48,7 +48,7 @@ class Ball {
     }
 
     if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
+      this.velY = (this.velY);
     }
 
     if ((this.y - this.size) <= 0) {
@@ -61,7 +61,7 @@ class Ball {
 
 collisionDetect() { 
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
